9712f50bc81ba70b90c47f44a958ed50edba1dfb
Aswin M <75745512+xyferas@users.noreply.github.com>
2026-01-15 09:09:54 +0530

    Restore Django settings file with default configuration

--- order_management/order_management/settings.py
+++ order_management/order_management/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
